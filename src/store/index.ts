d3648d412472de77218a034aa1d87af1cd85486f
xuech <xuechunhua@med-log.cn>
2021-02-04 17:43:38 +0800

    引入store

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+import { testData } from '../mock/testData'
+
+export default createStore({
+  state: {
+    columns: testData
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,14 @@
 <template>
   <div>
-        <column-list :list="list"></column-list>
+     <column-list :list="list"></column-list>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import ColumnList, { ColumnProps } from '../components/ColumnList.vue'
+import { computed, defineComponent } from 'vue'
+import ColumnList from '../components/ColumnList.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'iOS',
-    avatar: '',
-    description: '这是移动端'
-  },
-  {
-    id: 2,
-    title: 'Android',
-    avatar: '',
-    description: '这是移动端'
-  },
-  {
-    id: 3,
-    title: 'Vue',
-    avatar: '',
-    description: '这是PC端'
-  },
-  {
-    id: 4,
-    title: 'React',
-    avatar: '',
-    description: '这是PC端'
-  }
-]
+import { useStore } from 'vuex'
 
 export default defineComponent({
   name: 'Home',
@@ -41,8 +16,10 @@ export default defineComponent({
     ColumnList
   },
   setup () {
+    const store = useStore()
+    const list = computed(() => store.state.columns)
     return {
-      list: testData
+      list
     }
   }
 })
