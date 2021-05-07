02d17472c8f8b317decd11098c21cacb3b727d23
xuech <xuech_love@126.com>
2021-05-07 14:07:38 +0800

    change

---
+++ src/api/login.js
@@ -0,0 +1,26 @@
+import axios from '@/utils/request'
+
+/**
+ * 登录接口
+ * @param {*} loginInfo 用户登录信息
+ */
+const login = (loginInfo) => {
+  return axios.post('/login/login', {
+    ...loginInfo
+  })
+}
+
+/**
+ * 注册接口
+ * @param {*} regInfo 用户注册信息
+ */
+const reg = (regInfo) => {
+  return axios.post('/login/reg', {
+    ...regInfo
+  })
+}
+
+export {
+  login,
+  reg
+}
