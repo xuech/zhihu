fd1610a4492509a9c52ab3a0ccb21f28ed54e587
xuech <xuech_love@126.com>
2021-05-17 10:38:36 +0800

    修改配置

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,8 @@
       <div class="mb-3">
         <label class="form-label">邮箱地址</label>
         <validate-input
-          :rules="emailRules" v-model="emailVal"
+          :rules="emailRules"
+          v-model="emailVal"
           placeholder="请输入邮箱地址"
           type="text"
           ref="inputRef"
@@ -21,7 +22,10 @@
         />
       </div>
       <template #submit>
-        <button type="submit" class="btn btn-primary btn-block btn-large">登录</button>
+        <button
+          type="submit"
+          class="btn btn-primary btn-block btn-large"
+        >登录</button>
       </template>
     </validate-form>
   </div>
@@ -31,30 +35,30 @@
 import { defineComponent, ref } from 'vue'
 import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
 import ValidateForm from '../components/ValidateForm.vue'
-import { useRouter } from 'vue-router'
+// import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'Login',
   components: {
     ValidateInput,
-    ValidateForm
+    ValidateForm,
   },
-  setup () {
-    const router = useRouter()
+  setup() {
+    // const router = useRouter()
     const emailVal = ref('')
     const passwordVal = ref('')
     const emailRules: RulesProp = [
       { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
+      { type: 'email', message: '请输入正确的电子邮箱格式' },
     ]
     const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' }
+      { type: 'required', message: '密码不能为空' },
     ]
     const onFormSubmit = (result: boolean) => {
       if (result) {
         const payload = {
           email: emailVal.value,
-          password: passwordVal.value
+          password: passwordVal.value,
         }
         console.log('提交的参数', payload)
         // setTimeout(() => {
@@ -67,9 +71,8 @@ export default defineComponent({
       passwordVal,
       emailRules,
       passwordRules,
-      onFormSubmit
+      onFormSubmit,
     }
-  }
-
+  },
 })
 </script>
